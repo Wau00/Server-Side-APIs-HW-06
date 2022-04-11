bb2d748ad20f3eae6e686c16901c86734a282f1a
walterunderwood <walonsounderwood@gmail.com>
2022-04-11 01:52:48 -0700

    Modified and reorganized all elements of script.js

--- Assets/script.js
+++ Assets/script.js
@@ -1,10 +1,3 @@
-// Document Variables //
-var cityFormEl = document.querySelector('#cityFormEl');
-var cityInputEl = document.querySelector('#button-addon2');
-var weatherInfoEl = document.querySelector('#weatherInfo');
-var citySearchInput = document.querySelector('#city');
-var cityButtonEl = document.querySelector('#cities-button');
-
 // Setting Todays and Weekly Days //
 $("#thedate").text(moment().format("M/DD/YYYY"));
 $("#day-forecast").text(moment().add(1, 'day').format("M/DD/YYYY"));
@@ -13,49 +6,44 @@ $("#day-forecast3").text(moment().add(3, 'day').format("M/DD/YYYY"));
 $("#day-forecast4").text(moment().add(4, 'day').format("M/DD/YYYY"));
 $("#day-forecast5").text(moment().add(5, 'day').format("M/DD/YYYY"));
 
-//APIs Key for https://openweathermap.org/api //
 
+$(document).ready(function(){
+$('#submitWeather').click(function(){
+    var city = $('#button-city').val();
+    if(city !== ''){
 
-// Main function//
+        $.ajax({
+            url: 'https://api.openweathermap.org/data/2.5/weather?q='+ city + '&appid=e70a4a2e44d64a3b86dcb3ffc5b9cf15&units=imperial',
+            type: "GET" ,
+            statusCode: {
+                    404: function() {
+                    alert( "City not found" );
+                    }
+                },
+            dataType: "json",
+            success: function (data){
+                console.log(data);
+                var widget = show(data);
+                $('.card-properties').html(widget);
+                $('#button-city').val('');
 
-var formSubmit = function(event){
-    event.preventDefault();
-    var cities = cityInputEl.value.trim();
-    if(cities){
-        localWeather(cities);
-        cityInputEl.value ="";
-    }else{
-        alert("Please select a city");
-    }
-
-}
-
-// APIs  calls for Weather Attributes //
-var localWeather = function(cities){
-    var theKey = "e70a4a2e44d64a3b86dcb3ffc5b9cf15" ;
-    var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cities}&units=imperial&appid=${theKey}`
-
-    console.log(queryURL);
-    fetch(queryURL).then(function(response){
-        response.json().then(function(data){
-            displayForm(data, cities);
+                
+            }
         });
-    })
+
+        } else{
+            alert('Please enter a City');
+        }
+    });
+});
+
+function show(data){
+    return  $('.city-name').text(data.name)+
+            "<p style='text-transform: uppercase;'>"+data.weather[0].description+"<img src=http://openweathermap.org/img/wn/"+data.weather[0].icon+".png></img> </p>" +
+            "<p> Temperature : "+data.main.temp+" °F </p>" +
+            "<p> Humidity : "+data.main.humidity+" % </p>" +
+            "<p> Windspeed : "+data.wind.speed+ " mph </p>";
 };
 
-var displayForm = function(weather, searchCities){
-weatherInfoEl.textContent="";
-citySearchInput.textContent=searchCities;
 
-
-}
-
-
-
-
-
-cityFormEl.addEventListener("click", formSubmit);
-
-console.log(weatherInfo);
-console.log(city);
-
+// return "<p><strong>Weather</strong>: "+data.weather[0].main+"</p>" ;//
